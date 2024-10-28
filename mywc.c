21ed726f1728692ad7952f9f76d1d9f2f463b261
Matthias Haas <matthias.haas@students.fhv.at>
2024-10-28 07:56:34 +0000

    replaced if-requests with a switch-case

--- mywc.c
+++ mywc.c
@@ -9,25 +9,26 @@ int main(int argc, char *argv[]) {
 
     printf("Write some text: ");
 
-    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'c') {
-        while (getchar() != EOF) {
-            count++;
+    if (argc == 2 && argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 'c':
+                while (getchar() != EOF) {
+                    count++;
+                }
+                printf("\nNumber of characters: %d\n", count);
+                return 0;
+            case 'w':
+                char sign_prev = ' ';
+                char sign = getchar(); 
+                while (sign != EOF) {
+                    if(sign_prev != ' ' && sign == ' ') count++;
+                    sign_prev = sign;
+                    sign = getchar();
+                }
+                ++count;
+                printf("\nNumber of words: %d\n", count);
+                return 0;
         }
-        printf("\nNumber of characters: %d\n", count);
-        return 0;
     }
-    else if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'w') {
-        char sign_prev = ' ';
-        char sign = getchar(); 
-        while (sign != EOF) {
-            if(sign_prev != ' ' && sign == ' ') count++;
-            sign_prev = sign;
-            sign = getchar();
-        }
-        ++count;
-        printf("\nNumber of words: %d\n", count);
-        return 0;
-    }
-    
     return 0;
 }
